88abf92e55577f9e0dd752e44e345685eaddbf0d
Anderson Almeida <andersonpa10pa@gmail.com>
2024-03-16 10:42:26 -0300

    Configurações iniciais do projeto

---
+++ src/styles/global.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from "styled-components";
+import { stylesStandard } from "./stylesStandard";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    font-size: 62.5%;
+  }
+
+  :focus {
+    outline: 0;
+  }
+
+  body {
+    background: ${({ theme }) => theme["base-background"]};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    ${stylesStandard.fonts.textM}
+    color: ${({ theme }) => theme["base-text"]};
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button {
+    border: 0;
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  input {
+    border: 0;
+    outline: 0;
+  }
+`
---
+++ src/styles/stylesStandard.ts
@@ -0,0 +1,46 @@
+import { css } from 'styled-components'
+
+export const stylesStandard = {
+  fonts: {
+    titleL: css`
+      font-family: "Nunito", sans-serif;
+      font-size: 2.4rem;
+      line-height: 160%;
+      font-weight: 700;
+    `,
+
+    titleM: css`
+      font-family: "Nunito", sans-serif;
+      font-size: 2rem;
+      line-height: 160%;
+      font-weight: 700;
+    `,
+
+    textM: css`
+      font-family: "Nunito", sans-serif;
+      font-size: 1.6rem;
+      line-height: 160%;
+      font-weight: 400;
+    `,
+
+    textS: css`
+      font-family: "Nunito", sans-serif;
+      font-size: 1.4rem;
+      line-height: 160%;
+      font-weight: 400;
+    `,
+
+    textL: css`
+      font-family: 'Roboto', sans-serif;
+      font-size: 1.2rem,;
+      line-height: 160%;
+      font-weight: 400;
+    `,
+  },
+  widthScreen: {
+    screenXG: css`
+      width: 112rem;
+      margin: 0 auto;
+    `,
+  },
+}
