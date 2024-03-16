8898b15db73fbb6aadf25665e5219a9fc20d099c
Anderson Almeida <andersonpa10pa@gmail.com>
2024-03-16 10:42:49 -0300

    Criação do componente Header

---
+++ src/components/Header/style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import imageLogo from "../../assets/Cover.svg";
+
+export const HeaderContainer = styled.header`
+  width: 100vw;
+  height: 29vh;
+  background:  url(${imageLogo}) no-repeat center;
+`
